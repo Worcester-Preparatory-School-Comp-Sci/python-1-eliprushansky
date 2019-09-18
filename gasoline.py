29eac52c1c2efe3851cc0f3770bc6a0ee6eb2a66
eliprushansky <55286494+eliprushansky@users.noreply.github.com>
2019-09-18 11:22:41 -0400

    Update gasoline.py
    variable gallons added as user input for number of gallons
    variable liters added with its calculation
    variable barrels added with its calculation
    variable carbonDioxide added with its calculation
    variable price added with its calculation
    print number of gallons
    print number of liters
    print amount of co2 in pounds
    print price of gas in dollars
    
    call function gasoline

--- gasoline.py
+++ gasoline.py
@@ -1,6 +1,6 @@
 #ELI PRUSHANSKY 160919
 def gasoline():
-    #This function calculates different measurements of gasoline and its effects with a user input for the amount of gas
+    #This function calculates different measurements of gasoline and its effects with a user input for the amount of gasoline
     gallons=float(input("How many gallons?"))
     liters=float(gallons*3.785)
     barrels=float(gallons/19.5)
